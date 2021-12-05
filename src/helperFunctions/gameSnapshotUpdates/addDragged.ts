78d64662f577cfd09e079e9f8b8e9d7dcbb1751c
rabotnik1 <randall@posteo.de>
2021-12-05 15:37:32 +0100

    fixed bug of addDragged to GCZ not using cardRowShape

--- src/helperFunctions/gameSnapshotUpdates/addDragged.ts
+++ src/helperFunctions/gameSnapshotUpdates/addDragged.ts
@@ -1,5 +1,12 @@
 import { locate } from "../locateFunctions";
 import { produce } from "immer";
+import {
+  getCardGroupObjs,
+  getCardGroupObjsFromSnapshot,
+  getCardRowAndShape,
+  getCardRowShapeOnDraggedOver,
+  getCardRowShapeOnRearrange,
+} from "../groupGCZCards";
 
 function setAttributes(card: GameCard, attrs: { [key: string]: any }) {
   for (var key in attrs) {
@@ -11,10 +18,19 @@ export const addDragged = (gameSnapshot: GameSnapshot, sourceIndex: number, dest
   produce(gameSnapshot, draft => {
     const { player, place } = locate(destinationPlaceId, gameSnapshot);
     if (player !== null) {
+      let targetIndex = destinationIndex;
+      if (place === "GCZ") {
+        const enchantmentsRow = gameSnapshot.players[player].places["enchantmentsRow"];
+        const GCZ = gameSnapshot.players[player].places["GCZ"];
+
+        const cardGroupObjs = getCardGroupObjs( enchantmentsRow.cards, GCZ.cards);
+        const cardRowShape = getCardRowShapeOnDraggedOver(cardGroupObjs)
+        targetIndex = cardRowShape[destinationIndex];
+      }
       const newPlayerId = gameSnapshot.players[player].places[place].playerId;
-      setAttributes(draft.players[0].places.hand.cards[sourceIndex], { placeId: destinationPlaceId, playerId: newPlayerId });
+      setAttributes(draft.players[0].places.hand.cards[sourceIndex], { placeId: destinationPlaceId, playerId: newPlayerId, index: targetIndex });
       const [handCard] = draft.players[0].places.hand.cards.splice(sourceIndex, 1);
-      draft.players[player].places[place].cards.splice(destinationIndex, 0, handCard);
+      draft.players[player].places[place].cards.splice(targetIndex, 0, handCard);
       draft.players[player].places[place].cards = draft.players[player].places[place].cards.map((c, i) => ({ ...c, index: i }));
     }
   });
