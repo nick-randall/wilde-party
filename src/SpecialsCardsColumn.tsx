1d72c81ae91d360f93d66e5214517544add6d899
rabotnik1 <randall@posteo.de>
2021-12-06 10:23:44 +0100

    index and id of specialszone columns corrected

--- src/SpecialsCardsColumn.tsx
+++ src/SpecialsCardsColumn.tsx
@@ -15,10 +15,15 @@ export const SpecialsCardsColumn = (props: SpecialsCardsColumnProps) => {
   const highlights = useSelector((state: RootState) => state.highlights);
   const draggedHandCard = useSelector((state: RootState) => state.draggedHandCard);
   const specialsColumnType = cards[0].specialsCardType;
+  //
+  
+  const specialsColumnLastIndex = cards[cards.length - 1].index;
+  // Represents the next index where a new card of this special type will be inserted;
+  const specialsColumnId = specialsZoneId + (specialsColumnLastIndex + 1 );
   const isHighlighted = highlights.includes(specialsZoneId) && draggedHandCard?.specialsCardType === specialsColumnType;
-  const draggedOver = useSelector((state: RootState) => state.dragUpdate.droppableId===specialsZoneId + cards[cards.length - 1].index);
+  const draggedOver = useSelector((state: RootState) => state.dragUpdate.droppableId === specialsColumnId);
   const ghostCard = draggedHandCard && draggedOver ? draggedHandCard : undefined;
-
+  const allowDropping = isHighlighted
 
   return (
     <div style={{ display: "flex", flexDirection: "column-reverse", width: dimensions.cardWidth }}>
@@ -28,32 +33,26 @@ export const SpecialsCardsColumn = (props: SpecialsCardsColumnProps) => {
           <Card index={card.index} id={card.id} image={card.image} dimensions={dimensions} key={card.id} />
         </div>
       ))}
-      <div
-      //style={{ height: 30 }}
-      // Container
-      >
-        <Droppable droppableId={specialsZoneId + cards[cards.length - 1].index}>
-          {(provided, snapshot) => (
-            <div
-              ref={provided.innerRef}
-              {...provided.droppableProps}
-              style={{
-                height: dimensions.cardHeight + 30 * cards.length,
-                width: dimensions.cardWidth,
-                backgroundColor: snapshot.isDraggingOver ? "blue" : "",
-                //backgroundColor: isHighlighted ? "yellowgreen" : "",
-                boxShadow: isHighlighted ? "0px 0px 30px 30px yellowgreen" : "",
-                transition: "background-color 180ms, box-shadow 180ms, left 180ms",
-                position: "absolute",
-                bottom: -dimensions.cardHeight + 60,
-              }}
-            >
-              {provided.placeholder}
-              {ghostCard ? <GhostCard index={0} image={ghostCard.image} dimensions={dimensions} zIndex={9} /> : null}
-            </div>
-          )}
-        </Droppable>
-      </div>
+      <Droppable droppableId={specialsColumnId} >
+        {(provided) => (
+          <div
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            style={{
+              height: dimensions.cardHeight + 30 * cards.length + 30,
+              width: dimensions.cardWidth,
+              backgroundColor: isHighlighted ? "yellowgreen" : "",
+              boxShadow: isHighlighted ? "0px 0px 30px 30px yellowgreen" : "",
+              transition: "background-color 180ms, box-shadow 180ms, left 180ms",
+              position: "absolute",
+              bottom: - dimensions.cardHeight + 30,
+            }}
+          >
+            {provided.placeholder}
+            {ghostCard ? <GhostCard index={0} image={ghostCard.image} dimensions={dimensions} zIndex={9} /> : null}
+          </div>
+        )}
+      </Droppable>
     </div>
   );
 };
--- src/helperFunctions/gameSnapshotUpdates/addDragged.ts
+++ src/helperFunctions/gameSnapshotUpdates/addDragged.ts
@@ -17,6 +17,11 @@ function setAttributes(card: GameCard, attrs: { [key: string]: any }) {
 export const addDragged = (gameSnapshot: GameSnapshot, sourceIndex: number, destinationPlaceId: string, destinationIndex: number): GameSnapshot =>
   produce(gameSnapshot, draft => {
     const { player, place } = locate(destinationPlaceId, gameSnapshot);
+// if place is null check specialsZone
+    if (place === "specialsZone"){
+
+    }
+
     if (player !== null) {
       let targetIndex = destinationIndex;
       if (place === "GCZ") {
@@ -27,6 +32,7 @@ export const addDragged = (gameSnapshot: GameSnapshot, sourceIndex: number, dest
         const cardRowShape = getCardRowShapeOnDraggedOver(cardGroupObjs)
         targetIndex = cardRowShape[destinationIndex];
       }
+      
       const newPlayerId = gameSnapshot.players[player].places[place].playerId;
       setAttributes(draft.players[0].places.hand.cards[sourceIndex], { placeId: destinationPlaceId, playerId: newPlayerId, index: targetIndex });
       const [handCard] = draft.players[0].places.hand.cards.splice(sourceIndex, 1);
