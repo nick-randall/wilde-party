6e4460f50ff70e92271445c8fcd0616f057c69ae
rabotnik1 <randall@posteo.de>
2021-12-06 09:09:53 +0100

    fixed layout for specials column

--- src/SpecialsCardsColumn.tsx
+++ src/SpecialsCardsColumn.tsx
@@ -17,39 +17,35 @@ export const SpecialsCardsColumn = (props: SpecialsCardsColumnProps) => {
   const isHighlighted = highlights.includes(specialsZoneId) && draggedHandCard?.specialsCardType === specialsColumnType;
 
   return (
-    <div style={{ display: "flex", flexDirection: "column-reverse", width: dimensions.cardWidth }}>
-      <div style={{height: 30,width: dimensions.cardWidth}}>
+   <div style={{ display: "flex", flexDirection:"column-reverse"}}>
 
-      </div>
-      {cards.map(card => (
-        <div style={{ height: 30 }}>
-          <Card index={card.index} id={card.id} image={card.image} dimensions={dimensions} key={card.id} />
+    <Droppable droppableId={specialsZoneId + cards[cards.length - 1].index}>
+      {(provided) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.droppableProps}
+          style={{
+            zIndex: 9,
+            height: dimensions.cardHeight + (30 * cards.length - 1),
+            width: dimensions.cardWidth,
+            border: highlights.includes(specialsZoneId) ? "thick red dotted" : "",
+            backgroundColor: isHighlighted ? "yellowgreen" : "",
+            boxShadow: isHighlighted ? "0px 0px 30px 30px yellowgreen" : "",
+            transition: "background-color 180ms, box-shadow 180ms, left 180ms",
+          }}
+        >
+          <div>
+        
+          <div style={{ display: "flex", flexDirection: "column-reverse", width: dimensions.cardWidth }}>
+            {cards.map(card => (
+              <div style={{ height: 30 }}>
+                <Card index={card.index} id={card.id} image={card.image} dimensions={dimensions} key={card.id} />
+              </div>
+            ))}
+          </div></div>
         </div>
-      ))}
-      <div
-        //style={{ height: 30 }}
-        // Container
-      >
-        <Droppable droppableId={specialsZoneId + cards[cards.length - 1].index}>
-          {(provided, snapshot) => (
-            <div
-              ref={provided.innerRef}
-              {...provided.droppableProps}
-              style={{
-                zIndex: 9,
-                height: dimensions.cardHeight + (30 * cards.length - 1 ),
-                width: dimensions.cardWidth,
-                backgroundColor: snapshot.isDraggingOver ? "blue" : "",
-                //border: highlights.includes(specialsZoneId) ? "thick black solid" : "",
-                border: isHighlighted ? "thick black solid": "",
-                position: "absolute",
-              }}
-            >
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </div>
+      )}
+    </Droppable>
     </div>
   );
 };
