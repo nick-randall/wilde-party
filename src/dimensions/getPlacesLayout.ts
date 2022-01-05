08b55ad11c6ab89a5b5a65d40d5edb814c017f30
rabotnik1 <randall@posteo.de>
2022-01-05 15:21:18 +0100

    created PlayerLayout and PlacesLayout

--- src/dimensions/getPlacesLayout.ts
+++ src/dimensions/getPlacesLayout.ts
@@ -4,9 +4,12 @@ import { sortSpecials2 } from "../helperFunctions/getSpecialsOfType";
 import { getNumCards, locate } from "../helperFunctions/locateFunctions";
 import store, { RootState } from "../redux/store";
 
-export const getLayout = (id: string, screenSize: { width: number; height: number }, state: RootState | null = null): { x: number; y: number } => {
-  if (state === null) 
-  state =  store.getState();
+export const getPlacesLayout = (
+  id: string,
+  playerZoneSize: { width: number; height: number },
+  state: RootState | null = null
+): { x: number; y: number } => {
+  if (state === null) state = store.getState();
   const { gameSnapshot, dragUpdate, draggedHandCard } = state;
   const { player, place } = locate(id, gameSnapshot);
   const dimensions = getAllDimensions(id, gameSnapshot);
@@ -19,39 +22,22 @@ export const getLayout = (id: string, screenSize: { width: number; height: numbe
   if ((place === "specialsZone" || place === "UWZ") && player !== null) {
     const specialsZoneCards = gameSnapshot.players[player].places["specialsZone"].cards;
     const numSpecialsColumns = sortSpecials2(specialsZoneCards).length;
-    const numUWZColumns = 1; //gameSnapshot.players[player].places["UWZ"].cards.length > 0 ? 0 : 1;
+    // Always one UWZ column only
+    const numUWZColumns = 1;
     numCardsWidth = numSpecialsColumns + numUWZColumns;
   }
   if (place === "UWZ") draggedOverCard = 0;
-  if(place === "deck") numCards = 0;
+  if (place === "deck") numCards = 0;
 
   const fromCenterWidth = (distance: number): number =>
-    distance + (screenSize.width / 2 - ((numCardsWidth + draggedOverCard) * dimensions.cardWidth) / 2);
-  const fromCenterHeight = (distance: number): number => distance + (screenSize.height / 2 - dimensions.cardHeight / 2);
-  const fromRight = (distance: number) => screenSize.width - distance 
+    distance + (playerZoneSize.width / 2 - ((numCardsWidth + draggedOverCard) * dimensions.cardWidth) / 2);
+  const fromCenterHeight = (distance: number): number => distance + (playerZoneSize.height / 2 - dimensions.cardHeight / 2);
+  const fromRight = (distance: number) => playerZoneSize.width - distance;
   const handFromCenterWidth = (distance: number): number => {
-    const numHandCards = draggedHandCard ? numCards - 1 : numCards;
-    const rotation = (index: number) => 10 * index - (numHandCards / 2 - 0.5) * 10;
-    for (let i = 0; i < numCards; i++) {
-      //  console.log(widthOfRotated(rotation(i), cardWidth, cardHeight));
-    }
-    const firstCardRotation = rotation(1);
-    // console.log(firstCardRotation);
-    const firstCardWidth = widthOfRotated(-firstCardRotation, cardWidth, cardHeight);
-    // console.log(cardHeight, cardLeftSpread);
-    const cardsWiderThanUnRotated = firstCardWidth - cardWidth;
-    // console.log(firstCardWidth);
-    // console.log(document.getElementById("3w3323434")?.getBoundingClientRect().left);
-    // console.log(document.getElementById("gogogogo")?.getBoundingClientRect().right);
-    // console.log(cardsWiderThanUnRotated);
-    const handWidth = (numCards - 1) * cardLeftSpread - firstCardWidth;
-    const hw = (numCards - 1) * cardLeftSpread - cardWidth;
-    const hww = (cardLeftSpread / 2 - 0.5) * numCards; //(numCards - 1)
-
-    return distance + (screenSize.width / 2 - hww / 2);
+    const handWidth = (cardLeftSpread / 2 - 0.5) * numCards;
+    return distance + (playerZoneSize.width / 2 - handWidth / 2);
   };
-  const handFromBottom = (distance: number) => screenSize.height - dimensions.cardHeight - distance;
-
+  const handFromBottom = (distance: number) => playerZoneSize.height - dimensions.cardHeight - distance;
 
   if (player === 0) {
     switch (place) {
---
+++ src/dimensions/getPlayersLayout.ts
@@ -0,0 +1,44 @@
+import { getAllDimensions } from "../helperFunctions/getDimensions";
+import { locate } from "../helperFunctions/locateFunctions";
+import store, { RootState } from "../redux/store";
+
+const getPlayersLayout = (
+  screenSize: { width: number; height: number },
+  state: RootState | null = null,
+  playerId: string
+): { x: number; y: number; width: number; height: number } => {
+  if (state === null) state = store.getState();
+  const { gameSnapshot, dragUpdate, draggedHandCard } = state;
+  const { player } = locate(playerId, gameSnapshot);
+  const dimensions = getAllDimensions(playerId, gameSnapshot);
+  const { cardHeight, cardWidth, cardLeftSpread } = dimensions;
+
+  const playerZoneWidth = player === 0 ? 400 : 250;
+  const playerZoneHeight = player === 0 ? 700 : 450;
+  // need null case for deck and discardpile
+
+  const fromCenterWidth = (distance: number): number => distance + (screenSize.width / 2 - playerZoneWidth / 2);
+  const fromCenterHeight = (distance: number): number => distance + (screenSize.height / 2 - playerZoneHeight / 2);
+  const fromRight = (distance: number) => screenSize.width - distance;
+  const fromBottom = (distance: number) => screenSize.height - distance;
+
+  let playerZonePosition = { x: 0, y: 0 };
+
+  switch (player) {
+    case 0:
+      playerZonePosition = { x: fromCenterWidth(300), y: 200 };
+      break;
+    case 1:
+      playerZonePosition = { x: 200, y: 200 };
+      break;
+    case 2:
+      playerZonePosition = { x: 200, y: 500 };
+      break;
+    default:
+      playerZonePosition = { x: 0, y: 0 };
+      break;
+  }
+  return { ...playerZonePosition, height: playerZoneHeight, width: playerZoneWidth };
+};
+
+export default getPlayersLayout;
